2e7c06bf56588c3a64d1f07bf17839bfcffeed8f
Sashwat K <sashwat0001@gmail.com>
2020-09-12 23:14:24 +0530

    Made some documentation and fixed JSON error

--- arduino_nano_code/arduino_nano_code.ino
+++ arduino_nano_code/arduino_nano_code.ino
@@ -1,45 +1,60 @@
 /*
+ * Project Name : Home Weather Station
+ * Purpose : To get values from MQ-4, MQ-135, MQ-5 and MQ-2 and send to UNO via software serial
+ * Created on : 12 Sep 2020
+ * Created by : Sashwat K <sashwat0001@gmail.com>
+ * Revision : 1
+ * Last Updated by : Sashwat K <sashwat0001@gmail.com> 
+ * Last updated on : 12 Sep 2020
+ */
 
-*/
-#include <ArduinoJson.h>
-#include <SoftwareSerial.h>
+#include <ArduinoJson.h> // Library for JSON
+#include <SoftwareSerial.h> // Library for Software Serial
 
-SoftwareSerial mySerial(6, 7); //RX, TX
+SoftwareSerial sSerialToUNO(6, 7); //RX, TX
 
-const int mq4 = A0;
-const int mq135 = A1;
-const int mq5 = A2;
-const int mq2 = A3;
+const int mq4 = A0; // CNG gas
+const int mq135 = A1; // Air Quality Index
+const int mq5 = A2; // LPG gas
+const int mq2 = A3; // Smoke
 
 void setup() {
+  // Initialise sensors
   pinMode(mq4, INPUT);
   pinMode(mq135, INPUT);
   pinMode(mq5, INPUT);
   pinMode(mq2, INPUT);
+
+  // Initialise Serial
   Serial.begin(9600);
-  mySerial.begin(38400);
+  
+  while (!Serial) continue;
+  
+  sSerialToUNO.begin(38400);
 }
 
 void loop() {
   int mq4_value, mq135_value, mq5_value, mq2_value;
-  
+
+  // Get values from Sensor
   mq4_value = analogRead(mq4);
   mq135_value = analogRead(mq135);
   mq5_value = analogRead(mq5);
   mq2_value = analogRead(mq2);
-  
+
+  // Create JSON data
   StaticJsonDocument<200> doc;
   doc["mq4"] = mq4_value;
   doc["mq135"] = mq135_value;
   doc["mq5"] = mq5_value;
   doc["mq2"] = mq2_value;
-  
+  serializeJson(doc, sSerialToUNO); // Send JSON via Serial
+
+  // Display data for Debugging
   Serial.println("************************************");
   Serial.print("MQ-4 Value: ");Serial.print(mq4_value, DEC);Serial.println(" PPM");
   Serial.print("MQ-135 Value: ");Serial.print(mq135_value, DEC);Serial.println(" PPM");
   Serial.print("MQ-5 Value: ");Serial.print(mq5_value, DEC);Serial.println(" PPM");
   Serial.print("MQ-2 Value: ");Serial.print(mq2_value, DEC);Serial.println(" PPM");
   Serial.println("************************************\n\n");
-  
-  delay(1000);
 }
